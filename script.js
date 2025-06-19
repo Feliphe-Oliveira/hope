6a21a9f56a463d63810a54910496bfdc3a91b0ae
Feliphe-Oliveira <oliveirafeliphe272@gmail.com>
2025-06-19 17:23:40 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('btnHop').addEventListener('click',()=>{
+    const cod = 'pain'
+    if(document.getElementById('senha').value.toUpperCase()==cod.toUpperCase()){
+        document.getElementById('cx_verso').classList.add('destaque')
+        document.getElementById('login').classList.add('semdestaque')
+    } else if(document.getElementById('senha').value == ''){
+        alert('Prencha os dados solicitados')
+    } else(alert('Senha incorreta'))
+})
